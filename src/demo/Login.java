d4c7caef0564fa79698c3917a3f186a2e8165056
ADMIN <ADMIN@ADMIN-PC>
2020-01-05 09:44:56 +0530

    First repository

---
+++ src/demo/Login.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+	
+	
+	public Login()
+	{
+		System.out.println("default constructor");
+	}
+	
+	
+
+	 
+	
+}
+
---
+++ src/demo/check.java
@@ -0,0 +1,26 @@
+package demo;
+
+public class check{
+
+	
+	
+	public void rafi()
+	{
+		System.out.println("non parameter method");
+	}
+	 void rafi(String name, int pincode)
+	 {
+		 System.out.println(name+pincode);
+	 }
+	
+
+	
+	public static void main(String[] args) {
+		
+		check l = new check();
+		l.rafi();
+		l.rafi("mohammed", 518360);
+		
+	}
+	
+}
